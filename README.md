313c28d6dae64a09a81e72b894d006a1e750f943
Vlad <gregurco.vlad@gmail.com>
2017-10-24 12:52:34 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# GuzzleBundleCachePlugin
-CachePlugin for EightPointsGuzzleBundle
+Guzzle Bundle Cache Plugin
+==================
+
+[![Build Status](https://travis-ci.org/gregurco/GuzzleBundleCachePlugin.svg?branch=master)](https://travis-ci.org/gregurco/GuzzleBundleCachePlugin) [![Coverage Status](https://coveralls.io/repos/gregurco/GuzzleBundleCachePlugin/badge.svg?branch=master)](https://coveralls.io/r/gregurco/GuzzleBundleCachePlugin)
+
+This plugin integrates [Cache][1] functionality into Guzzle Bundle, a bundle for building RESTful web service clients.
+
+
+Requirements
+------------
+ - PHP 7.0 or above
+ - [Guzzle Bundle][2]
+
+ 
+Installation
+------------
+Using [composer][3]:
+
+``` json
+{
+    "require": {
+        "gregurco/guzzle-bundle-cache-plugin": "dev-master"
+    }
+}
+```
+
+
+Usage
+-----
+Load plugin in AppKernel.php:
+``` php
+new EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle([
+    new Gregurco\Bundle\GuzzleBundleCachePlugin\GuzzleBundleCachePlugin(),
+])
+```
+
+Configuration in config.yml:
+``` yaml
+eight_points_guzzle:
+    clients:
+        api_payment:
+            base_url: "http://api.domain.tld"
+
+            # define headers, options
+
+            # plugin settings
+            plugin:
+                cache:
+                    enabled: true
+                    strategy: "strategy_service_id" # optional
+```
+
+License
+-------
+This middleware is licensed under the MIT License - see the LICENSE file for details
+
+[1]: http://www.xml.com/pub/a/2003/12/17/dive.html
+[2]: https://github.com/8p/EightPointsGuzzleBundle
+[3]: https://getcomposer.org/
---
+++ src/GuzzleBundleCachePlugin.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Gregurco\Bundle\GuzzleBundleCachePlugin;
+
+
+use EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundlePlugin;
+use Gregurco\Bundle\GuzzleBundleCachePlugin\DependencyInjection\GuzzleCacheExtension;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\ExpressionLanguage\Expression;
+
+class GuzzleBundleCachePlugin extends Bundle implements EightPointsGuzzleBundlePlugin
+{
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $extension = new GuzzleCacheExtension();
+        $extension->load($configs, $container);
+    }
+
+    /**
+     * @param array $config
+     * @param ContainerBuilder $container
+     * @param string $clientName
+     * @param Definition $handler
+     */
+    public function loadForClient(array $config, ContainerBuilder $container, string $clientName, Definition $handler)
+    {
+        if ($config['enabled']) {
+            $cacheMiddlewareDefinitionName = sprintf('guzzle_bundle_cache_plugin.middleware.%s', $clientName);
+            $cacheMiddlewareDefinition = new Definition('%guzzle_bundle_cache_plugin.middleware.class%');
+
+            if ($config['strategy']) {
+                $cacheMiddlewareDefinition->addArgument(new Reference($config['strategy']));
+            }
+
+            $container->setDefinition($cacheMiddlewareDefinitionName, $cacheMiddlewareDefinition);
+
+            $cacheMiddlewareExpression = new Expression(sprintf('service("%s")', $cacheMiddlewareDefinitionName));
+
+            $handler->addMethodCall('push', [$cacheMiddlewareExpression]);
+        }
+    }
+
+    /**
+     * @param ArrayNodeDefinition $pluginNode
+     */
+    public function addConfiguration(ArrayNodeDefinition $pluginNode)
+    {
+        $pluginNode
+            ->canBeEnabled()
+            ->children()
+                ->scalarNode('strategy')->defaultNull()->end()
+            ->end();
+    }
+
+    /**
+     * @return string
+     */
+    public function getPluginName() : string
+    {
+        return 'cache';
+    }
+}
